7e2932943f227917e929f19fd61ab77ede7fc3fa
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-01-08 13:21:40 +0530

    Contacts Data Fetched Success

--- src/app/api/connection/contacts/route.ts
+++ src/app/api/connection/contacts/route.ts
@@ -1,9 +1,15 @@
 import { NextRequest } from "next/server";
-
+import { pool } from "@/lib/db";
 export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams;
     const searchId = searchParams.get("c");
 
     console.log("Search Id for Contacts:", searchId);
-    return Response.json({ message: "Server Side Success" }, { status: 200 })
+    const fetch = await pool.query(`select * from "MessagePermission" where ("senderId" = $1 or "recieverId" = $1) and status='accepted'`, [searchId]);
+    if(fetch.rows.length === 0)
+        return Response.json({message:"No Contacts Found"},{status:200});
+
+    const contacts = fetch.rows;
+    console.log("Contacts Fetched", contacts)
+    return Response.json({ message: "Server Side Success", Contacts: contacts }, { status: 200 })
 }
