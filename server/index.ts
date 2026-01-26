10c316103f377fb39c1e421df7820f4ee3d68508
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-01-27 00:28:26 +0530

    ok

--- server/index.ts
+++ server/index.ts
@@ -142,7 +142,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
   console.log(`Socket server running on ${PORT}`);
