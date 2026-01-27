e19dfcf45c2ff998e37151e1a1128cadb3424592
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-01-27 13:09:10 +0530

    Final Submission

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const io = new Server(server, {
   },
 });
 
-const UserMapping = new Map<string, string>();
+const UserMapping = new Map();
 
 io.on("connection", (socket) => {
   console.log("New client connected:", socket.id);
