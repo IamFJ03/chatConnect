ad9cec2bb89e7df3df44ecdddd80d53b7d413bea
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-01-27 13:14:29 +0530

    Final Fixes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import { pool } from "@/lib/db";
+import { pool } from "../src/lib/db";
 
 const app = express();
 
