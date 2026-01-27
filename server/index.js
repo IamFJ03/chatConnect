bde6f9934745410ec904220dfdab18610d69c08f
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-01-27 13:28:20 +0530

    Path Fixes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import { pool } from "../src/lib/db";
+import { pool } from "./db";
 
 const app = express();
 
